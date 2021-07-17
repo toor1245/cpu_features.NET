d274a74debfbb071b8d6bac286d1d622d8cc3b18
Mykola Hohsadze <koliagogsadze@gmail.com>
2021-07-18 02:52:52 +0300

    add support for Linux

--- src/CpuFeaturesDotNet/Imports/DllPath.cs
+++ src/CpuFeaturesDotNet/Imports/DllPath.cs
@@ -2,6 +2,11 @@ namespace CpuFeaturesDotNet.Imports
 {
     internal static class DllPath
     {
-        public const string CPU_FEATURES_DOTNET_DLL = "Imports\\cpu_features_dotnet.dll";
+        public static string Path = System.IO.Path.Combine("Imports", "cpu_features_dotnet");
+#if OS_LINUX
+        public const string CPU_FEATURES_DOTNET_DLL = "Imports/cpu_features_dotnet";
+#elif OS_WINDOWS
+        public const string CPU_FEATURES_DOTNET_DLL = "Imports\\cpu_features_dotnet";
+#endif
     }
 }
