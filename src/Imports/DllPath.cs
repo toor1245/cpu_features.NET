8096bc6d958646a6a1c9a13d54c529ad5b069e6c
Mykola Hohsdze <koliagogsadze@gmail.com>
2021-07-18 04:05:06 +0300

    add macos path to import library

--- src/Imports/DllPath.cs
+++ src/Imports/DllPath.cs
@@ -3,7 +3,7 @@ namespace CpuFeaturesDotNet.Imports
     internal static class DllPath
     {
         public static string Path = System.IO.Path.Combine("Imports", "cpu_features_dotnet");
-#if OS_LINUX
+#if OS_LINUX || OS_MAC
         public const string CPU_FEATURES_DOTNET_DLL = "Imports/cpu_features_dotnet";
 #elif OS_WINDOWS
         public const string CPU_FEATURES_DOTNET_DLL = "Imports\\cpu_features_dotnet";
