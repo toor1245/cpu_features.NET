04df741ee9af9d743e17dc10b12054679b2e941a
Nikolay Hohsadze <43444946+toor1245@users.noreply.github.com>
2021-07-25 20:16:14 +0300

    Implement unit tests for MemoryUtils, StringUtils, BitUtils (#2)
    * add unit tests for MemoryUtils, StringUtils, BitUtils and add check null
    
    * fix dotnet-format StringView
    
    * fix dotnet-format for unit testing project

---
+++ tests/Utils/MemoryUtilsTests.cs
@@ -0,0 +1,116 @@
+using CpuFeaturesDotNet.Utils;
+using Xunit;
+
+namespace CpuFeaturesDotNet.UnitTesting.Utils
+{
+    public unsafe class MemoryUtilsTests
+    {
+        [Fact]
+        public void memchr_FindCharacter_True()
+        {
+            // Arrange
+            var sse41 = stackalloc byte[]
+            {
+                0x49,
+                0x49,
+                0x41,
+                0x22,
+                0x5F,
+                0x1F
+            };
+            const int expected = 0x41;
+
+            // Act
+            var length = (uint)StringUtils.str_len(sse41);
+            var actual = (byte*)MemoryUtils.memchr(sse41, 0x41, length);
+
+            // Assert
+            Assert.Equal(expected, *actual);
+        }
+
+        [Fact]
+        public void memchr_LengthNull_True()
+        {
+            // Arrange
+            var sse41 = stackalloc byte[]
+            {
+                0x49,
+                0x49,
+                0x41,
+                0x22,
+                0x5F,
+                0x1F
+            };
+
+            // Act
+            var actual = (byte*)MemoryUtils.memchr(sse41, 0x41, 0);
+
+            // Assert
+            Assert.True(actual == null);
+        }
+
+        [Fact]
+        public void memcmp_CompareStrings_True()
+        {
+            // Arrange
+            var sse41 = stackalloc byte[]
+            {
+                0x49,
+                0x49,
+                0x41,
+                0x22,
+                0x5F,
+                0x1F
+            };
+
+            var sse42 = stackalloc byte[]
+            {
+                0x49,
+                0x49,
+                0x41,
+                0x22,
+                0x5F,
+                0x20
+            };
+            var expected = -1;
+
+            // Act
+            var actual = MemoryUtils.memcmp(sse41, sse42, (uint)StringUtils.str_len(sse41));
+
+            // Assert
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void memcmp_CompareStrings_False()
+        {
+            // Arrange
+            var sse41 = stackalloc byte[]
+            {
+                0x49,
+                0x49,
+                0x41,
+                0x22,
+                0x5F,
+                0x1F
+            };
+
+            var sse42 = stackalloc byte[]
+            {
+                0x49,
+                0x49,
+                0x41,
+                0x22,
+                0x5F,
+                0x1F
+            };
+            var expected = 0;
+
+            // Act
+            var actual = MemoryUtils.memcmp(sse41, sse42, (uint)StringUtils.str_len(sse41));
+
+            // Assert
+            Assert.Equal(expected, actual);
+        }
+    }
+}
---
+++ tests/Utils/StringUtilsTests.cs
@@ -0,0 +1,64 @@
+using CpuFeaturesDotNet.Utils;
+using Xunit;
+
+namespace CpuFeaturesDotNet.UnitTesting.Utils
+{
+    public unsafe class StringUtilsTests
+    {
+        [Fact]
+        public void strlen_GetLengthOfString_ReturnsSix()
+        {
+            // Arrange
+            var sse41 = stackalloc byte[]
+            {
+                0x49,
+                0x49,
+                0x41,
+                0x22,
+                0x5F,
+                0x1F
+            };
+            const ulong expected = 6UL;
+
+            // Act
+            var actual = StringUtils.str_len(sse41);
+
+            // Assert
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void strlen_GetLengthOfString_ReturnsLength()
+        {
+            // Arrange
+            var sse3 = stackalloc byte[]
+            {
+                0x49,
+                0x49,
+                0x41,
+                0x21
+            };
+            const ulong expected = 4UL;
+
+            // Act
+            var actual = StringUtils.str_len(sse3);
+
+            // Assert
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void strlen_Empty_True()
+        {
+            // Arrange
+            var str = stackalloc byte[] { };
+            const ulong expected = 0UL;
+
+            // Act
+            var actual = StringUtils.str_len(str);
+
+            // Assert
+            Assert.Equal(expected, actual);
+        }
+    }
+}
