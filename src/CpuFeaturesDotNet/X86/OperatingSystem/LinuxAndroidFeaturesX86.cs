d8d39d241cb544188c5b023806f91fc06cdd2d66
Mykola Hohsdze <koliagogsadze@gmail.com>
2021-07-31 04:53:34 +0300

    fix stackalloc

--- src/CpuFeaturesDotNet/X86/OperatingSystem/LinuxAndroidFeaturesX86.cs
+++ src/CpuFeaturesDotNet/X86/OperatingSystem/LinuxAndroidFeaturesX86.cs
@@ -1,5 +1,6 @@
 using System.Runtime.CompilerServices;
 using CpuFeaturesDotNet.IO;
+using CpuFeaturesDotNet.Utils;
 using static CpuFeaturesDotNet.X86.CpuInfoX86.FeaturesX86;
 
 namespace CpuFeaturesDotNet.X86.OperatingSystem
@@ -13,25 +14,7 @@ namespace CpuFeaturesDotNet.X86.OperatingSystem
             {
                 return;
             }
-            var flags = stackalloc byte[]
-            {
-                0x42,
-                0x6C,
-                0x3D,
-                0x43,
-                0x49
-            };
             StackLineReader.Initialize(out var reader, fileDescriptor);
-            var featuresString = stackalloc byte[6]
-            {
-                0x49,
-                0x49,
-                0x41,
-                0x00,
-                0x00,
-                0x00
-            };
-
             while (true)
             {
                 var result = StackLineReader.NextLine(ref reader);
@@ -42,60 +25,20 @@ namespace CpuFeaturesDotNet.X86.OperatingSystem
                 {
                     continue;
                 }
-
-                if (!StringView.IsEquals(key, StringView.GetString(flags)))
+                
+                if (!StringView.IsEquals(key, StringView.GetString(StringUtils.GetAsciiBytes("flags"))))
                 {
                     continue;
                 }
 
-                IsSupportedSSE = StringView.HasWord(value, featuresString, 3);
-                IsSupportedSSE2 = HasSse2(in value, featuresString);
-                IsSupportedSSE3 = HasSse3(in value, featuresString);
-                IsSupportedSSSE3 = HasSsse3(in value, featuresString);
-                IsSupportedSSE41 = HasSse41(in value, featuresString);
-                IsSupportedSSE42 = HasSse42(in value, featuresString);
+                IsSupportedSSE = StringView.HasWord(value, StringUtils.GetAsciiBytes("sse"));
+                IsSupportedSSE2 = StringView.HasWord(value, StringUtils.GetAsciiBytes("sse2"));
+                IsSupportedSSE3 = StringView.HasWord(value, StringUtils.GetAsciiBytes("sse3"));
+                IsSupportedSSSE3 = StringView.HasWord(value, StringUtils.GetAsciiBytes("ssse3"));
+                IsSupportedSSE41 = StringView.HasWord(value, StringUtils.GetAsciiBytes("sse4_1"));
+                IsSupportedSSE42 = StringView.HasWord(value, StringUtils.GetAsciiBytes("sse4_2"));
                 break;
             }
-        }
-
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        private static unsafe bool HasSse2(in StringView value, byte* featuresString)
-        {
-            featuresString[3] = 0x20;
-            return StringView.HasWord(value, featuresString, 4);
-        }
-
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        private static unsafe bool HasSse3(in StringView value, byte* featuresString)
-        {
-            featuresString[3] = 0x21;
-            return StringView.HasWord(value, featuresString, 4);
-        }
-
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        private static unsafe bool HasSsse3(in StringView value, byte* featuresString)
-        {
-            featuresString[2] = 0x49;
-            featuresString[3] = 0x41;
-            featuresString[4] = 0x21;
-            return StringView.HasWord(value, featuresString, 5);
-        }
-
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        private static unsafe bool HasSse41(in StringView value, byte* featuresString)
-        {
-            featuresString[2] = 0x41;
-            featuresString[3] = 0x22;
-            featuresString[4] = 0x5F;
-            featuresString[5] = 0x1F;
-            return StringView.HasWord(value, featuresString, 6);
-        }
-
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        private static unsafe bool HasSse42(in StringView value, byte* featuresString)
-        {
-            featuresString[5] = 0x20;
-            return StringView.HasWord(value, featuresString, 6);
         }
     }
 }
