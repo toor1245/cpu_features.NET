c1a022161f464d651d92eed938872fe7749359e3
Mykola Hohsdze <koliagogsadze@gmail.com>
2021-08-01 15:17:30 +0300

    test

--- tests/ArmTests/CpuInfoArmTests.cs
+++ tests/ArmTests/CpuInfoArmTests.cs
@@ -1,12 +1,11 @@
 using CpuFeaturesDotNet.Native;
-using CpuFeaturesDotNet.UnitTesting.Attributes;
 using Xunit;
 
 namespace CpuFeaturesDotNet.UnitTesting.Arm
 {
     public class CpuInfoArmTests
     {
-        [FactArm]
+        [Fact]
         public void IsArchArmAny_True()
         {
             Assert.False(Architecture.IsArchArmAny());
