6d8651af012d7f62f551729c7876e850807d5280
Mykola Hohsadze <koliagogsadze@gmail.com>
2021-08-07 17:23:35 +0300

    add ci/cd

--- scripts/run_integration.sh
+++ scripts/run_integration.sh
@@ -1,3 +1,4 @@
-dotnet --version
-dotnet build "src/CpuFeaturesDotNet" -c Release --force
-dotnet test "tests" -c Release
+#!/usr/bin/env bash
+
+docker run --rm --privileged multiarch/qemu-user-static:register --reset
+docker run -it --rm toor1245/cpu_features-dotnet:manifest-arm32v7
