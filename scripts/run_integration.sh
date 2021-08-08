0cb11b41f84caef416bb85140b4dc252e72cff7a
Mykola Hohsadze <koliagogsadze@gmail.com>
2021-08-08 03:50:16 +0300

    test actions

--- scripts/run_integration.sh
+++ scripts/run_integration.sh
@@ -1,4 +1,6 @@
-#!/usr/bin/env bash
+#sudo apt-get remove packages-microsoft-prod
+#sudo apt-get remove 'dotnet*' 'aspnet*' 'netstandard*'
+#sudo apt-get install dotnet-sdk-5.0
 
-docker run --rm --privileged multiarch/qemu-user-static:register --reset
-docker run -it --rm toor1245/cpu_features-dotnet:manifest-arm32v7
+sudo docker run --rm --privileged multiarch/qemu-user-static:register --reset
+sudo docker run -it --rm toor1245/cpu_features-dotnet:manifest-arm32v7
