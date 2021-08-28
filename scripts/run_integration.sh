1caaa81c1f696b39db180bf8124d459cdfafe6b7
Mykola Hohsdze <koliagogsadze@gmail.com>
2021-08-28 20:38:14 +0300

    remove redundant files

--- scripts/run_integration.sh
+++
@@ -1,6 +0,0 @@
-#sudo apt-get remove packages-microsoft-prod
-#sudo apt-get remove 'dotnet*' 'aspnet*' 'netstandard*'
-#sudo apt-get install dotnet-sdk-5.0
-
-sudo docker run --rm --privileged multiarch/qemu-user-static:register --reset
-sudo docker run -it --rm toor1245/cpu_features-dotnet:manifest-arm32v7
