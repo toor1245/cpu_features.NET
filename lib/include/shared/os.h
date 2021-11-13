b3288b5e79d19582cb955ff056a2025ea830e5f1
Nikolay Hohsadze <koliagogsadze@gmail.com>
2021-11-13 16:08:08 +0200

    Fix darwin tests (#24)

--- lib/include/shared/os.h
+++ lib/include/shared/os.h
@@ -13,6 +13,7 @@
 #elif CPU_FEATURES_DOTNET_OS_LINUX_OR_ANDROID
 #elif CPU_FEATURES_DOTNET_OS_FREEBSD
 #elif CPU_FEATURES_DOTNET_OS_DARWIN
+#include <sys/sysctl.h>
 #else
 #error "Unsupported OS"
 #endif  // CPU_FEATURES_DOTNET_OS
@@ -29,4 +30,5 @@ CPU_FEATURES_DOTNET_DLL_EXPORT bool is_os_linux_android(void);
 CPU_FEATURES_DOTNET_DLL_EXPORT bool is_os_android(void);
 CPU_FEATURES_DOTNET_DLL_EXPORT bool is_os_darwin(void);
 CPU_FEATURES_DOTNET_DLL_EXPORT bool is_os_freebsd(void);
+CPU_FEATURES_DOTNET_DLL_EXPORT bool darwin_sysctlbyname(char* name);
 CPU_FEATURES_DOTNET_DLL_EXPORT bool windows_is_processor_feature_present(int32_t ProcessorFeature);
