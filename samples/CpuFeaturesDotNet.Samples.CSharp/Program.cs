256a2faafc2e5224bc22742ed0b89629b5b83375
Mykola Hohsdze <Mykola_Hohsadze@epam.com>
2022-06-21 22:09:24 +0300

    add arm64 demo

---
+++ samples/CpuFeaturesDotNet.Samples.CSharp/Program.cs
@@ -0,0 +1,12 @@
+using System;
+using CpuFeaturesDotNet.AArch64;
+
+var aarch64Info = Aarch64Info.GetAarch64Info();
+
+Console.WriteLine($"Implementer: {aarch64Info.Implementer}");
+Console.WriteLine($"Part: {aarch64Info.Part}");
+Console.WriteLine($"Revision: {aarch64Info.Revision}");
+Console.WriteLine($"Variant: {aarch64Info.Variant}");
+Console.WriteLine($"IsSupportedASIMD: {aarch64Info.Features.IsSupportedASIMD}");
+Console.WriteLine($"IsSupportedCPUID: {aarch64Info.Features.IsSupportedCPUID}");
+Console.WriteLine($"IsSupportedCPUID: {aarch64Info.Features.IsSupportedFP}");
